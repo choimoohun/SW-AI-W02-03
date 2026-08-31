06953ddd2648c4d8c7ebae0f75e44feb7960dde1
choimoohun <271946661+choimoohun@users.noreply.github.com>
2026-08-31 15:43:57 +0900

    병합 정렬
    (cherry picked from commit b6e6b09101c0706e8a61fc581da71c1c1e8cc928)

--- week2/2. advanced/02_merge_sort.py
+++ week2/2. advanced/02_merge_sort.py
@@ -33,19 +33,38 @@ def merge(arr, left, mid, right):
         right: 오른쪽 부분의 끝 인덱스
     """
     # TODO: 왼쪽과 오른쪽 부분 배열을 임시 배열로 복사
-    pass
+    l = arr[left:mid + 1]
+    r = arr[mid + 1:right + 1]
     
     # TODO: 두 배열을 병합
     pass
     
     
     # TODO: left_arr와 right_arr를 비교하며 작은 값을 arr에 복사
-    pass
+    i = j = 0
+    k = left
+
+    while i < len(l) and j < len(r):
+        if l[i] <= r[j]:
+            arr[k] = l[i]
+            i += 1
+        else:
+            arr[k] = r[j]
+            j += 1
+        k += 1
     
     # TODO: 남은 원소들을 복사
     # left_arr에 남은 원소가 있으면 복사
     # right_arr에 남은 원소가 있으면 복사
-    pass
+    while i < len(l):
+        arr[k] = l[i]
+        i += 1
+        k += 1
+
+    while j < len(r):
+        arr[k] = r[j]
+        j += 1
+        k += 1
 
 def merge_sort_helper(arr, left, right):
     """
@@ -61,7 +80,12 @@ def merge_sort_helper(arr, left, right):
     ## 왼쪽 절반 재귀 정렬
     ## 오른쪽 절반 재귀 정렬
     ## 정렬된 두 절반을 병합
-    pass
+    if left >= right:
+        return
+    mid = (left + right) // 2
+    merge_sort_helper(arr, left, mid)
+    merge_sort_helper(arr, mid + 1, right)
+    merge(arr, left, mid, right)
 
 def merge_sort(arr):
     """
